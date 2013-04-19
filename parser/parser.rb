8f40c2f34a073fc85dab48777c91152b79f2782b
stefan-kolb <stefan-kolb@web.de>
2013-04-20 00:16:07 +0200

    demo

--- parser/parser.rb
+++ parser/parser.rb
@@ -1,13 +1,17 @@
 require './commands/move'
 require './commands/gamestart'
 require './commands/chat'
+require './commands/commands'
+
 require 'bindata'
+require 'zlib'
 
 class Header < BinData::Record
 	endian :little
 	
   int32 :header_len
-	skip :length => lambda { header_len - 4 }
+	int32 :next_header
+	string :data, :length => lambda { header_len - 8 }
 end
 
 class Command < BinData::Record
@@ -29,11 +33,20 @@ class Action < BinData::Record
 	endian :little
 	
 	int32 :len
-	skip :length => lambda { len + 4 }
+	
+	uint8 :cmd
+
+  #choice :action, :selection => :cmd do
+  #  move Commands::MOVE
+  #end
+	#skip :length => 4
+	string :data, :length => lambda { len + 3 }
 end
 
 io = File.open('rec.mgx')
-Header.read(io)
+head_comp = Header.read(io)
+#uncompressed_data = Zlib::Inflate.new(-Zlib::MAX_WBITS).inflate(head_comp.data)
+#puts uncompressed_data
 
 until BinData::Record::count_bytes_remaining == 0 do
 	ope = Command.read(io)
@@ -55,13 +68,66 @@ until BinData::Record::count_bytes_remaining == 0 do
 			gets
 		end
 	elsif ope.type == 2
-		puts "2"
 		Sync.read(io)
 	elsif ope.type == 1
-		puts "1"
-		Action.read(io)
+		a = Action.read(io)
+		
+		case a.cmd
+		when Commands::ATTACK
+			puts "Attack"
+		when Commands::MOVE
+			puts "Move"
+		when 	Commands::RESIGN
+			puts Commands::RESIGN
+			puts "Resign"
+		when Commands::STOP
+			puts "Stop"
+		when Commands::DELETE
+			puts "Delete"
+		when Commands::GROUNDATTACK
+			puts "Groundattack"
+		when Commands::TRIBUTE
+			puts "Tribute"
+		when Commands::UNGARRISON
+			puts "UNgarrison"
+		when Commands::SELL
+			puts "Sell"
+		when Commands::BUY
+			puts "Buy"
+		when Commands::TOWNBELL
+			puts "Townbell"
+		when Commands::AGGRO
+			puts "Aggro"
+		when Commands::GUARD
+			puts "Guard"
+		when Commands::FOLLOW
+			puts "Follow"
+		when Commands::PATROL
+			puts "Patrol"
+		when Commands::FORMATION
+			puts "Formation"
+		when Commands::TECH
+			puts "Tech"
+		when Commands::BUILD
+			puts "Build"
+		when Commands::GAMESPEED
+			puts "Gamespeed"
+		when Commands::FLARE
+			puts "Flare"
+		when Commands::GARRISON
+			puts "Garrison"
+		when Commands::TRAIN
+			puts "Train"
+		when Commands::GATHER
+			puts "Gather"
+		when Commands::BACKTOWORK
+			puts "Backtowork"
+		else
+			puts "You gave me #{a.cmd} -- I have no idea what to do with that."
+			gets
+		end
 	else
-		puts "?"
+		puts "? error!"
 		gets
 	end
 end
